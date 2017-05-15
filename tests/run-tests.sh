b75563ef0c427c021235a1cbe7454869770495ef
Luke Horwell <luke@ubuntu-mate.org>
2017-05-15 22:27:48 +0100

    Prepare foundation for tests.
    Re-uses structure from Welcome.

---
+++ tests/run-tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Test ubuntu-mate-welcome locally.
+#
+# These parameters specify the length of the test:
+# --quick
+# --extended
+#
+
+repo_root=$(dirname "$0")/../
+success=true
+cd "$repo_root"
+
+# Determine which tests to run
+tests=""
+if [ "$1" == "--quick" ]; then
+    tests=""
+
+elif [ "$1" == "--extended" ]; then
+    for test in "$repo_root"/tests/*.py; do
+        tests+=" $(basename $test)"
+    done
+
+else
+    echo "Please specify one of the following:"
+    echo " "
+    echo " --quick      Checks for common errors and faults."
+    echo " --extended   Deep test, checks remote repositories."
+    exit 1
+fi
+
+# Perform each test
+echo -e "\n-------- Start of Testing --------"
+for test in $tests; do
+    python3 "$repo_root"/tests/"$test"
+    if [ ! "$?" == "0" ]; then
+        success=false
+    fi
+done
+echo -e "\n-------- End of Testing  --------\n"
+
+if [ "$success" == true ]; then
+    exit 0
+else
+    exit 1
+fi
