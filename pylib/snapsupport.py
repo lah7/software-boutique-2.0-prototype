f9d18490c617b9bd4abde7a4462be6f429beae0a
Luke Horwell <luke@ubuntu-mate.org>
2017-06-28 18:04:10 +0100

    Show authentication prompts in snap module.

--- pylib/snapsupport.py
+++ pylib/snapsupport.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
-import getpass
 import gi
 import json
 import os
 import sys
+import subprocess
+from shutil import which
 gi.require_version ('Snapd', '1')
 from gi.repository import Snapd
 
 # Connects on-demand
 client = None
+
+# Check we have Zenity installed.
+zenity_present = which("zenity")
+if not zenity_present:
+    print("Zenity is not installed.")
+    exit(1)
 
 
 def progress_snap_cb (client, change, deprecated, user_data):
@@ -46,14 +53,65 @@ def save_auth_data(auth_data):
 
 
 def snap_login():
-    username = input('Username:')
-    password = getpass.getpass(prompt='Password:')
+    def _ask_for_username():
+        # Invite user to sign up for account.
+        try:
+            question = subprocess.check_output(["zenity", "--question", \
+            "--title", "Authentication for Snappy", \
+            "--text", "To install snaps from the Ubuntu Store (without being root), you need an Ubuntu One account.\n\nDo you have an Ubuntu One account?", \
+            "--ok-label", "Continue", \
+            "--cancel-label", "Sign up"]).decode("ascii")
+        except subprocess.CalledProcessError:
+            # Zenity produces exit code 1 on cancel.
+            # Presume cancelled
+            subprocess.Popen(["xdg-open", "https://login.ubuntu.com/"])
+
+        # Prompt for username
+        try:
+            username = subprocess.check_output(["zenity", "--entry", \
+                "--title", "Authentication for Snappy", \
+                "--text", "Please enter your e-mail address for your Ubuntu One account."]).decode('ascii').replace('\n', '')
+            return username
+        except subprocess.CalledProcessError:
+            subprocess.call(["zenity", "--error", \
+            "--title", "Authentication for Snappy", \
+            "--text", "No credentials were supplied. This snap cannot be installed."])
+            return None
+
+    def _ask_for_password():
+        # Prompt for password
+        try:
+            password = subprocess.check_output(["zenity", "--password", \
+            "--title", "Authentication for Snappy", \
+            "--text", "Please enter your Ubuntu One password."]).decode('ascii').replace('\n', '')
+            return password
+        except subprocess.CalledProcessError:
+            subprocess.call(["zenity", "--error", \
+            "--title", "Authentication for Snappy", \
+            "--text", "No credentials were supplied. This snap cannot be installed."])
+            return None
+
+    def _ask_for_2FA():
+        # Get 2FA
+        try:
+            twofactorauth = subprocess.check_output(["zenity", "--entry", \
+            "--title", "Authentication for Snappy", \
+            "--text", "Please enter your 2 factor authentication code."]).decode('ascii').replace('\n', '')
+            return twofactorauth
+        except subprocess.CalledProcessError:
+            subprocess.call(["zenity", "--error", \
+            "--title", "Authentication for Snappy", \
+            "--text", "No credentials were supplied. This snap cannot be installed."])
+            return None
+
+    username = _ask_for_username()
+    password = _ask_for_password()
 
     try:
         auth_data = Snapd.login_sync(username, password, None)
     except Exception as e:
         if e.domain == 'snapd-error-quark' and e.code == Snapd.Error.TWO_FACTOR_REQUIRED:
-            otp = input('OTP:')
+            otp = _ask_for_2FA()
             try:
                 auth_data = Snapd.login_sync(username, password, otp)
             except:
