2f878f372ca006bd3041883daebd9eecab8fbf50
Luke Horwell <code@horwell.me>
2018-01-14 19:00:11 +0000

    Update copyright e-mail.

--- pylib/preferences.py
+++ pylib/preferences.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
 #
-# Copyright 2015-2018 Luke Horwell <luke@ubuntu-mate.org>
+# Copyright 2015-2018 Luke Horwell <code@horwell.me>
 #
 # Software Boutique is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
